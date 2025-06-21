3fc52a2808066381c036d232d6f463abad2656ff
David Smith <davidsmith@hamiltonfour.tech>
2025-06-21 17:55:30 -0400

    removing mypy for now

--- eiplgrader/languages/executors/templates.py
+++ eiplgrader/languages/executors/templates.py
@@ -11,7 +11,7 @@ import json
 from typing import Any, Dict, List, Optional
 from .string_utils import (
     CodeBuilder,
-    dedent_template: str,
+    dedent_template,
     escape_string_literal,
 )
 
